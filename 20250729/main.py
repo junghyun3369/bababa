e1c407a441e6c2fd914952c39443c6817ffe4518
junghyun3369 <leesang3369@gmail.com>
2025-07-29 13:30:38 +0900

    CRUD

--- 20250729/main.py
+++ 20250729/main.py
@@ -1,3 +1,42 @@
 import test
+#  CRUD > 입력,읽기,수정,삭제 > 태이블(NOTIC)
+# test.c("insert into NOTICE (title) value ('2025')")
+while True:
+    print("\n--- [게시판 관리 프로그램] ---")
+    print("1. 게시글 추가")
+    print("2. 게시글 수정")
+    print("3. 게시글 삭제")
+    print("4. 전체 게시글 보기")
+    print("5. 종료")
+    choice = input("무엇을 원하나요? ")
 
-
+    if choice == '1':    
+        title = input("제목 입력")
+        test.c(f"insert into NOTICE (title) value ('{title}')")
+        test.r('select * from NOTICE')
+    elif choice == '2':
+        #  수정 실행
+        print("\n수정사항이 들어왔네요,일 할 시간이군요. 마음껏 둘러보시길.")
+        test.r('select * from NOTICE')
+        uuu = input("자, 새로 태어나고 싶은 당신을 말하세요.")
+        newtitle = input("그것의 숫자를 말해주세요.")
+        sql = f"update NOTICE set title = '{uuu}' where no = {newtitle}"
+        test.u(sql)
+        #  수정 내용 확인
+        test.r('select * from NOTICE')
+    elif choice == '3':
+        # 삭제 실행
+        print("\n지우고 싶으신게 있으신가요?")
+        test.r('select * from NOTICE') 
+        ddd = input("그 숫자를 말해주세요.")
+        sql = f"delete from NOTICE where no = {ddd}"
+        test.d(sql)
+        # 삭제 내용 확인
+        test.r('select * from NOTICE')
+    elif choice == '4':
+        test.r('select * from NOTICE')
+    elif choice == '5':
+        print("별에게 축복 있기를.")
+        break
+    else:
+        print("다른걸 원하시나요?")
--- 20250729/test.py
+++ 20250729/test.py
@@ -13,7 +13,40 @@ print(conn_params)
 
 conn = mariadb.connect(**conn_params);
 
-cur = conn.cursor() # 선택
+
+def r(sql):
+	try:
+		cur = conn.cursor() # 선택
+		# sql = 'select * from NOTICE'
+		cur.execute(sql) # ㄴ위의 질의어를 실행하기
+		result = cur.fetchall() #결과값 받기
+		print(result) #결과 출력
+	except mariadb.ProgrammingError as e:
+    		print(e)
+
+def c(sql):
+	try:	
+		cur = conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+	except mariadb.ProgrammingError as e:
+				print(e)
+
+def u(sql):
+	try:	
+		cur = conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+	except mariadb.ProgrammingError as e:
+				print(e)
+
+def d(sql):
+	try:	
+			cur = conn.cursor()
+			cur.execute(sql)
+			conn.commit()
+	except mariadb.ProgrammingError as e:
+				print(e)
 
 if __name__ == "__main__":
     print(conn_params)
